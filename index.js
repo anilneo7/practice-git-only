58b13d0f92521f2cf4c9d40ec651487e3d150280
anilneo7 <104692794+anilneo7@users.noreply.github.com>
2024-08-01 17:08:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const age=24;
 const name1='joy new';
 const data="some code added in earlier code in order to practice stashing";
+const data1="new data";
 const dob='01/01/2002';
 
 function sayName(){
